5976e860710cd1a2c923212db2fd249ef11c72b2
Tom <thunt8nrn@gmail.com>
2020-10-26 19:45:33 +0000

    changed use of sun

--- Driver.java
+++ Driver.java
@@ -9,14 +9,14 @@ public class Driver
 
         Planet[] planets = new Planet[8];
 
-        planets[0] = new Planet("#DBDBDB", 6, 4, 55, 180, sun);             //Mercury
-        planets[1] = new Planet("#DBDBDB", 8, 3.5, 70, 180, sun);           //Venus
-        planets[2] = new Planet("#1A85FF", 10, 3, 100, 180, sun);           //Earth
-        planets[3] = new Planet("#C1440E", 7, 2.5, 145, 180, sun);          //Mars
-        planets[4] = new Planet("#FFCB78", 30, 2, 200, 180, sun);           //Jupiter
-        planets[5] = new Planet("#EDA353", 28, 1.5, 280, 180, sun);         //Saturn
-        planets[6] = new Planet("#8594A3", 14, 1, 325, 180, sun);           //Uranus
-        planets[7] = new Planet("#1C70C8", 14, 0.5, 370, 180, sun);         //Neptune
+        planets[0] = new Planet("#DBDBDB", 6, 4, 55, 180);             //Mercury
+        planets[1] = new Planet("#DBDBDB", 8, 3.5, 70, 180);           //Venus
+        planets[2] = new Planet("#1A85FF", 10, 3, 100, 180);           //Earth
+        planets[3] = new Planet("#C1440E", 7, 2.5, 145, 180);          //Mars
+        planets[4] = new Planet("#FFCB78", 30, 2, 200, 180);           //Jupiter
+        planets[5] = new Planet("#EDA353", 28, 1.5, 280, 180);         //Saturn
+        planets[6] = new Planet("#8594A3", 14, 1, 325, 180);           //Uranus
+        planets[7] = new Planet("#1C70C8", 14, 0.5, 370, 180);         //Neptune
 
 
         while(true)
@@ -25,7 +25,7 @@ public class Driver
             
             for(int i=0; i<8; i++)
             {
-                ss.drawSolarObjectAbout(planets[i].returnDistanceFromSun(),planets[i].returnAngleFromSun(),planets[i].returnDiameter(),planets[i].returnColour(), planets[i].returnSunX(),planets[i].returnSunY());
+                ss.drawSolarObjectAbout(planets[i].returnDistanceFromSun(),planets[i].returnAngleFromSun(),planets[i].returnDiameter(),planets[i].returnColour(), sun.returnXPosition(),sun.returnYPosition());
             }
             
             ss.finishedDrawing();
